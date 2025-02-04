11e7349507ecbbef5ff369fcf7224a6d4b6e9622
PRIYA-ANGHAN <priyaanghan934@gmail.com>
2025-02-04 17:42:19 +0530

    add new logic

--- custom_solar/custom_solar/doctype/watt_peaks/watt_peaks.py
+++ custom_solar/custom_solar/doctype/watt_peaks/watt_peaks.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2025, varoon soneji and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class WattPeaks(Document):
 	pass
+
+@frappe.whitelist()
+def update_same_price(new_price, panel_type):
+    # Update only records with the same panel_type
+    frappe.db.sql("""
+        UPDATE `tabWatt Peaks`
+        SET panel_price = %s
+        WHERE panel_type = %s
+    """, (new_price, panel_type))
+    
+    frappe.db.commit()
+    return True
