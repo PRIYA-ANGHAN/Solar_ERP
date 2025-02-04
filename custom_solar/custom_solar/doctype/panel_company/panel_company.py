da8b577fb8a253cd39fd4a7b04e51acae132aefc
PRIYA-ANGHAN <priyaanghan934@gmail.com>
2025-02-04 11:23:39 +0530

    add new dotype

---
+++ custom_solar/custom_solar/doctype/panel_company/panel_company.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2025, varoon soneji and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PanelCompany(Document):
+	pass
+
+# panel company
+#add new doc
